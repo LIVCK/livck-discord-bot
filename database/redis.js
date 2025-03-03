6e3746de674314605ab7e470d53f73c234c4bb05
Rene-Roscher <roscher794@gmail.com>
2025-03-03 16:48:17 +0100

    fix supervisord

--- database/redis.js
+++ database/redis.js
@@ -1,7 +1,7 @@
 import redis from 'redis';
 
 const client = redis.createClient({
-    url: `redis://${process.env.REDIS_PASSWORD}@${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`
+    url: `redis://${process.env.REDIS_PASSWORD}@${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`,
 });
 
 client.on('connect', () => { console.log('Connected to Redis') });
