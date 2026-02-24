45aa7f133b013ec2e25eedae8e4f784eea54e573
Rene Roscher <roscher794@gmail.com>
2026-02-24 02:32:18 +0100

    feat(role-mentions): add RoleMention model to the database initialization

--- models/index.js
+++ models/index.js
@@ -3,12 +3,14 @@ import Subscription from "./Subscription.js";
 import Statuspage from "./Statuspage.js";
 import Message from "./Message.js";
 import CustomLink from "./CustomLink.js";
+import RoleMention from "./RoleMention.js";
 
 const models = {
     Subscription: Subscription(database),
     Statuspage: Statuspage(database),
     Message: Message(database),
     CustomLink: CustomLink(database),
+    RoleMention: RoleMention(database),
 };
 
 Object.keys(models).forEach((modelName) => {
